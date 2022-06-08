ae7bcf40ed73162de46f8060c2a4aad503d99cda
yenohp <devinanderson286@gmail.com>
2022-06-08 12:37:52 -0700

    Script pseudo

---
+++ script.js
@@ -0,0 +1,26 @@
+const gameBoard = (() => {
+    // Create a two dimensional array
+    // display empty array -- in tic-tac-toe format
+    // update via inputs
+})();
+
+const gameOver = (() => {
+    // Create players based on modal inputs
+    // Allow for input clicks on tic-tac-toe
+    // Game logic - legal move is only able to click on non used space
+    // If all spaces are full and no winning move has been made, tie
+    // Else if three spaces are filled that align (maybe index wise?), win
+    // On gameOver, prompt player with rematch or reset
+})();
+const displayController = (() => {
+    // New Game button eventlistener creates modal for player creation
+    // displays player names/computer
+    // displays game event (victory/tie)
+    // changes score
+    // updates on each input
+})();
+
+const player = (name, score) => {
+    // display name -- and score?
+    // increment score
+}
